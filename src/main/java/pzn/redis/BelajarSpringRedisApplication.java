22ad0019e7770eac5beb7a3f93cde54d5ad48b03
arfinorestualam <arfino.alam@gmail.com>
2024-12-30 21:58:34 +0700

    first

---
+++ src/main/java/pzn/redis/BelajarSpringRedisApplication.java
@@ -0,0 +1,13 @@
+package pzn.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelajarSpringRedisApplication.class, args);
+    }
+
+}
