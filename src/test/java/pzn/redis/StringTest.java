9b2d0c2e2a8a4848ad7054e369ac9c8d930824d5
arfinorestualam <arfino.alam@gmail.com>
2024-12-31 23:46:51 +0700

    testing value operation

--- src/test/java/pzn/redis/StringTest.java
+++ src/test/java/pzn/redis/StringTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.time.Duration;
 
 @SpringBootTest
 public class StringTest {
@@ -15,5 +18,17 @@ public class StringTest {
     @Test
     void redisTemplate() {
         Assertions.assertNotNull(template);
+    }
+
+    //test for using value operation
+    @Test
+    void string() throws InterruptedException {
+        ValueOperations<String, String> ops = template.opsForValue();
+
+        ops.set("name", "world", Duration.ofSeconds(2));
+        Assertions.assertEquals("world", ops.get("name"));
+
+        Thread.sleep(Duration.ofSeconds(3));
+        Assertions.assertNull(ops.get("name"));
     }
 }
