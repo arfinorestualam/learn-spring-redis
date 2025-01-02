a06a4ea8974320e10255572af195d96c3e48c740
arfinorestualam <arfino.alam@gmail.com>
2025-01-02 17:23:12 +0700

    learn how to use and manipulate list operation

--- src/test/java/pzn/redis/RedisTest.java
+++ src/test/java/pzn/redis/RedisTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
 import java.time.Duration;
 
 @SpringBootTest
-public class StringTest {
+public class RedisTest {
     //this test is created to see, if redis template automatically create or not
     @Autowired
     private StringRedisTemplate template;
@@ -32,5 +33,24 @@ public class StringTest {
 
         Thread.sleep(Duration.ofSeconds(3).toMillis());
         Assertions.assertNull(ops.get("name"));
+    }
+
+    //test for using and manipulate list operation
+    @Test
+    void list() {
+        //opForList() is method for list operation in redis template
+        ListOperations<String, String> ops = template.opsForList();
+        //to add list in the right, using rightPush, names as key, and value with the data
+        ops.rightPush("names", "fin");
+        ops.rightPush("names", "world");
+        ops.rightPush("names", "fi");
+        //the data of the list will be fin,world,fi
+
+        //to get data and release from the left, using leftPop
+        Assertions.assertEquals("fin", ops.leftPop("names"));
+        Assertions.assertEquals("world", ops.leftPop("names"));
+        Assertions.assertEquals("fi", ops.leftPop("names"));
+
+
     }
 }
